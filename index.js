7ce225686bf26c8e33cfc41a4434f97867bca7a5
catherinegay <papascat@aol.com>
2023-06-03 18:43:02 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code here
+
+class Polygon{
+    constructor(array){
+        this.array = array
+    }
+    get countSides(){
+        return this.array.length
+    }
+    get perimeter(){
+        let tt=0;
+        for(let i=0;i<this.array.length; i++){
+            tt+= this.array[i];
+        }
+        return tt;
+    //     return this.array.reduce((acc, count)=>{
+    //         acc + count
+    //     },0)
+    // }
+
+}}
+class Triangle extends Polygon{
+   // super(array)
+    get isValid(){
+        if(this.array[0]+ this.array[1] > this.array[2] && this.array[1]+this.array[2] > this.array[0] && this.array[2] + this.array[0] > this.array[1])
+        return true; //(this.array[0] + this.array[1]) > this.array[2] ? true : false;
+        else 
+        return false;
+    }
+    
+}
+class Square extends Polygon{
+    get isValid(){
+        return (this.array[0] ===this.array[1]) ? true : false;
+    }
+    get area(){
+        return Math.pow(this.array[0],2)
+
+    }
+}
